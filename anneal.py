e1845fe40985864f466ffd34b909783ae1264277
SebBehrendtC:/Users/Sebas/OneDrive/Documents/venvs/simpy_venv/Scripts/python.exe <sebastian.behrendt@kit.edu>
2022-10-06 00:36:13 +0200

    Improved annealing
    Former-commit-id: 826dae3ba3bf0de2256d18e6ecf91545bb4d356c

--- anneal.py
+++ anneal.py
@@ -6,6 +6,8 @@ import loader
 import print_util
 from post_processing import PostProcessor
 
+from copy import deepcopy
+
 
 from evolutionary import check_valid_configuration, random_configuration, evaluate, mutation, crossover
 from simanneal import Annealer
@@ -15,7 +17,7 @@ import json
 
 from util import set_seed
 
-SEED=21
+SEED=22
 
 SAVE_FOLDER = "data/anneal_results"
 
@@ -46,23 +48,29 @@ class ProductionSystemOptimization(Annealer):
             configuration = mutation(scenario_dict=scenario_dict, individual=[self.state])[0][0]
             base_configuration = loader.CustomLoader()
             base_configuration.read_data(base_scenario, "json")
-            if configuration.to_dict() in solutions:
-                print("already tested solution")
             if check_valid_configuration(configuration=configuration, base_configuration=base_configuration, scenario_dict=scenario_dict) and configuration.to_dict() not in solutions:
-                solutions.append(configuration.to_dict())
-                self.counter += 1
-                # print("Found valid Move", self.counter)
                 self.state = configuration
-                configuration.to_json(f"{SAVE_FOLDER}/f_{str(self.counter)}.json")
                 break
 
     def energy(self):
+        if self.state.to_dict() in solutions:
+            print("already simulated")
+            index = solutions.index(solutions)
+            return performances["00"][str(index)]["agg_fitness"]
+        
+        solutions.append(self.state.to_dict())
+        self.state.to_json(f"{SAVE_FOLDER}/f_{str(self.counter)}.json")
+        # print("Found valid Move", self.counter)
+
         values = evaluate(scenario_dict=scenario_dict, base_scenario=base_scenario, individual=[self.state])
         performance = sum([value*weight for value, weight in zip(values, weights)])
         print("Evaluted ind", self.counter, "for value:", performance)
         performances["00"][str(self.counter)] = {'agg_fitness': performance, "fitness": [float(value) for value in values]}
         with open("data/anneal_results.json", "w") as json_file:
             json.dump(performances, json_file)
+
+        self.counter += 1
+
         return performance
 
 initial_state = loader.CustomLoader()
