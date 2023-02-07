9f0fdfd87711b9463e0895d25f0679e9fc2a6926
susip <usecz@student.kit.edu>
2023-02-07 15:01:37 +0100

    set_objective_function zu optimize hinzugefügt get_opening_cost_of_stations gefixed versucht get_modul_counts zu fixen (falls Modul in Ausgangskonfiguration noch nicht vorhanden)
    Former-commit-id: a57d54ea5642ae9a5f1b67e645d125be7fc6fdd9

--- prodsim/util/math_opt.py
+++ prodsim/util/math_opt.py
@@ -15,12 +15,16 @@ from gurobipy import GRB
 
 def get_modul_counts(adapter: adapters.Adapter) -> Dict[str, int]:
     modul_count_dict = {}
+    # Fall Prozessmodul noch nicht vorhanden fehlt
+    for process in adapter.process_data:
+        if isinstance(process, processes_data.ProductionProcessData):
+            modul_count_dict[process] = 0
     for resource in adapter.resource_data:
         if not isinstance(resource, resource_data.ProductionResourceData):
             continue
         for process in resource.processes:
-            if not process in modul_count_dict.keys():
-                modul_count_dict[process] = 0
+            # if not process in modul_count_dict.keys():
+            #    modul_count_dict[process] = 0
             modul_count_dict[process] += 1
 
     return modul_count_dict
@@ -47,10 +51,10 @@ class MathOptimizer(BaseModel):
     def cost_module(self, x: int, Modul: str) -> int:
         module_cost = self.scenario_dict["costs"]["process_module"]
         return module_cost * (x - get_modul_counts(self.adapter)[Modul])
-        
+
     def set_variables(
-        self,
-    ):  
+            self,
+    ):
         self.processing_times_per_product_and_step = (
             self.get_processing_times_per_product_and_step()
         )
@@ -74,9 +78,8 @@ class MathOptimizer(BaseModel):
                 for step in product_type.processes:
                     x[product_type.ID][work_piece_index][step] = {}
                     for station in range(
-                        self.scenario_dict["constraints"]["max_num_machines"]
-                    ):  
-                        
+                            self.scenario_dict["constraints"]["max_num_machines"]
+                    ):
                         x[product_type.ID][work_piece_index][step][station] = self.model.addVar(
                             vtype=GRB.BINARY,
                             name="x[{},{},{},{}]".format(
@@ -96,36 +99,35 @@ class MathOptimizer(BaseModel):
             for station in range(self.scenario_dict["constraints"]["max_num_machines"])
         ]
         return process_modules, stations
-    
+
     def get_opening_cost_of_stations(self):
         opening_cost = {}
         _, stations = self.get_process_modules_and_stations()
         num_previous_machines = len(adapters.get_machines(self.adapter))
         for counter, station in enumerate(stations):
-            if counter <= num_previous_machines:
+            if counter < num_previous_machines:
                 opening_cost[station] = 0
             else:
                 opening_cost[station] = self.scenario_dict["costs"]["machine"]
         return opening_cost
 
-
     def set_objective_function(
-        self,
+            self,
     ):
         process_modules, stations = self.get_process_modules_and_stations()
         opening_costs = self.get_opening_cost_of_stations()
         objective = (
-            sum(self.t[modul] for modul in process_modules)
-            + sum(opening_costs[station] * self.s[station] for station in stations)
-            + sum(
-                self.v[station] * self.scenario_dict["costs"]["breakdown_cost"]
-                for station in stations
-            )
+                sum(self.t[modul] for modul in process_modules)
+                + sum(opening_costs[station] * self.s[station] for station in stations)
+                + sum(
+            self.v[station] * self.scenario_dict["costs"]["breakdown_cost"]
+            for station in stations
+        )
         )
         self.model.setObjective(objective, GRB.MINIMIZE)
 
     def set_constraints(
-        self,
+            self,
     ):
         self.check_available_station_for_workpieces()
         self.check_available_station_for_workpieces()
@@ -142,7 +144,7 @@ class MathOptimizer(BaseModel):
                     for station, variable in stations.items():
                         self.model.addConstr(
                             variable - self.s[station] <= 0,
-                            "für_{}_{}_{}_durchgeführt_an_{}".format(
+                            "für_{}_Werkstück_{}_{}_durchgeführt_an_Station_{}".format(
                                 product, workpiece, process_step, station
                             ),
                         )
@@ -157,7 +159,7 @@ class MathOptimizer(BaseModel):
                             for station in stations.keys()
                         )
                         == 1,
-                        "für_{}_{}_wird_{}_durchgeführt".format(
+                        "für_{}_Werkstück_{}_wird_{}_durchgeführt".format(
                             product, workpiece, process_step
                         ),
                     )
@@ -169,11 +171,11 @@ class MathOptimizer(BaseModel):
                     for station in stations.keys():
                         self.model.addConstr(
                             (
-                                self.x[product][workpiece][process_step][station]
-                                - self.z[process_step, station]
-                                <= 0
+                                    self.x[product][workpiece][process_step][station]
+                                    - self.z[process_step, station]
+                                    <= 0
                             ),
-                            "für_{}_{}_{}_vorhanden_an_{}".format(
+                            "für_{}_Werkstück_{}_{}_vorhanden_an_Station_{}".format(
                                 product, workpiece, process_step, station
                             ),
                         )
@@ -240,20 +242,20 @@ class MathOptimizer(BaseModel):
         for station in range(self.scenario_dict["constraints"]["max_num_machines"]):
             self.model.addConstr(
                 (
-                    (
                         (
-                            self.s[station]
-                            * machine_breakdown_count
-                            * machine_breakdown_time
-                        )
-                        + sum(
+                                (
+                                        self.s[station]
+                                        * machine_breakdown_count
+                                        * machine_breakdown_time
+                                )
+                                + sum(
                             self.z[Modul, station]
                             * module_breakdown_count[Modul]
                             * module_breakdown_time[Modul]
                             for Modul in module_breakdown_count
                         )
-                    )
-                    == self.a[station]
+                        )
+                        == self.a[station]
                 ),
                 "Berechnung_der_Ausfallzeit_von_{}".format(station),
             )
@@ -282,18 +284,18 @@ class MathOptimizer(BaseModel):
         for station in range(self.scenario_dict["constraints"]["max_num_machines"]):
             self.model.addConstr(
                 (
-                    (
-                        gp.quicksum(
-                            self.processing_times_per_product_and_step[product][step]
-                            * self.x[product][workpiece][step][station]
-                            for product, workpieces in self.x.items()
-                            for workpiece, process_steps in workpieces.items()
-                            for step in process_steps.keys()
+                        (
+                            gp.quicksum(
+                                self.processing_times_per_product_and_step[product][step]
+                                * self.x[product][workpiece][step][station]
+                                for product, workpieces in self.x.items()
+                                for workpiece, process_steps in workpieces.items()
+                                for step in process_steps.keys()
+                            )
                         )
-                    )
-                    + self.a[station]
-                    - BZ
-                    <= 0
+                        + self.a[station]
+                        - BZ
+                        <= 0
                 ),
                 "Berechnung_der_überschrittenen_Zeit_an_{}".format(station),
             )
@@ -303,11 +305,11 @@ class MathOptimizer(BaseModel):
         for module in process_modules:
             self.model.addConstr(
                 (
-                    self.cost_module(
-                        sum(self.z[module, station] for station in stations), module
-                    )
-                    - self.t[module]
-                    <= 0
+                        self.cost_module(
+                            sum(self.z[module, station] for station in stations), module
+                        )
+                        - self.t[module]
+                        <= 0
                 ),
                 "Sicherstellen_Maximum_{}".format(module),
             )
@@ -316,28 +318,28 @@ class MathOptimizer(BaseModel):
         _, stations = self.get_process_modules_and_stations()
         self.model.addConstr(
             (
-                sum(self.v[Station] for Station in stations)
-                - self.scenario_dict["maximum_breakdown_time"]
-                <= 0
+                    sum(self.v[Station] for Station in stations)
+                    - self.scenario_dict["maximum_breakdown_time"]
+                    <= 0
             ),
             "Maximale_Ausfallzeit_einhalten",
         )
 
     def optimize(
-        self,
-    ):  
+            self,
+    ):
         st = datetime.datetime.now()
         self.model: Any = gp.Model("MILP_Rekonfiguration")
 
-
         self.set_variables()
         self.set_constraints()
+        self.set_objective_function()
 
         # Optimierung
         stopt = datetime.datetime.now()
         self.model.optimize()
         end = datetime.datetime.now()
-        print(end-stopt)
+        print(end - stopt)
 
         status = self.model.Status
         if status == GRB.UNBOUNDED:
@@ -349,17 +351,16 @@ class MathOptimizer(BaseModel):
 
         elapsed_time = end - st
         print('Execution time:', elapsed_time, 'seconds')
-
+        print(self.model.write("MILP.lp"))
         # TODO: store results for later export or postprocessing
 
-
     def save_model(
-        self,
+            self,
     ):
         pass
         # self.adapters.resource_data.append(resource_data.ProductionResourceData())
 
     def save_result_to_adapter(
-        self,
+            self,
     ):
         pass
