869831332ca6e84100d75b92365f24dc40a09dff
SebBehrendtC:/Users/Sebas/OneDrive/Documents/venvs/simpy_venv/Scripts/python.exe <sebastian.behrendt@kit.edu>
2023-04-06 14:25:33 +0200

    Move Anneyling to function
    Former-commit-id: 281f9a6375f1711c6665aa0940bd2e06cd38b7c2

--- anneal_example.py
+++ anneal_example.py
@@ -1,94 +1,13 @@
-import json
-import time
-from copy import deepcopy
-
-from simanneal import Annealer
-
-from prodsim.simulation import sim
-from prodsim import adapters
-from prodsim.util.optimization_util import (
-    check_valid_configuration,
-    crossover,
-    evaluate,
-    mutation,
-    random_configuration,
-    document_individual,
-    get_weights,
-)
-from prodsim.util.util import set_seed
+from prodsim.util.simulated_annealing import run_simulated_annealing
 
 SEED = 22
-sim.VERBOSE = 0
-
 SAVE_FOLDER = "data/anneal_results"
 BASE_CONFIGURATION_FILE_PATH = "examples/optimization_example/base_scenario.json"
 SCENARIO_FILE_PATH = "examples/optimization_example/scenario.json"
 
-base_configuration = adapters.JsonAdapter()
-base_configuration.read_data(BASE_CONFIGURATION_FILE_PATH, SCENARIO_FILE_PATH)
+Tmax = 10000
+Tmin = 0.67
+steps = 4000
+updates = 300
 
-set_seed(SEED)
-
-
-# weights für: (throughput, wip, cost)
-# weights = (-0.004, 1.0, 0.0003)
-# weights = (-0.025, 1.0, 0.001)
-weights = get_weights(base_configuration, "min")
-
-performances = {}
-performances["00"] = {}
-solution_dict = {"current_generation": "00", "00": []}
-start = time.perf_counter()
-
-# TODO: get Annealer packakge, include in requirements and import here
-class ProductionSystemOptimization(Annealer):
-    def __init__(self, initial_state=None, load_state=None):
-        super().__init__(initial_state, load_state)
-
-    def move(self):
-        while True:
-            # print("Move")
-            configuration = mutation(individual=[deepcopy(self.state)])[0][0]
-            if check_valid_configuration(
-                configuration=configuration,
-                base_configuration=base_configuration,
-            ):
-                self.state = configuration
-                break
-
-    def energy(self):
-
-        values = evaluate(
-            base_scenario=base_configuration,
-            performances=performances,
-            solution_dict=solution_dict,
-            individual=[self.state],
-        )
-
-        performance = sum([value * weight for value, weight in zip(values, weights)])
-        # print("\n\t########## Evaluted ind", self.counter, "for value:", performance)
-        counter = len(performances["00"]) - 1
-        document_individual(solution_dict, SAVE_FOLDER, [self.state])
-        performances["00"][str(counter)] = {
-            "agg_fitness": performance,
-            "fitness": [float(value) for value in values],
-            "time_stamp": time.perf_counter() - start,
-        }
-        with open("data/anneal_results.json", "w") as json_file:
-            json.dump(performances, json_file)
-
-        return performance
-
-
-pso = ProductionSystemOptimization(initial_state=base_configuration)
-
-# pso.auto(minutes=240)
-# {'tmax': 7500.0, 'tmin': 0.67, 'steps': 1500, 'updates': 100} 64:27:53ä'
-
-
-pso.Tmax = 10000
-pso.Tmin = 0.67
-pso.steps = 4000
-pso.updates = 300
-
-internary, performance = pso.anneal()
+run_simulated_annealing(SAVE_FOLDER, BASE_CONFIGURATION_FILE_PATH, SCENARIO_FILE_PATH, SEED, Tmax, Tmin, steps, updates)
---
+++ prodsim/util/simulated_annealing.py
@@ -0,0 +1,111 @@
+import json
+import time
+from copy import deepcopy
+
+from simanneal import Annealer
+
+from prodsim.simulation import sim
+from prodsim import adapters
+from prodsim.util.optimization_util import (
+    check_valid_configuration,
+    crossover,
+    evaluate,
+    mutation,
+    random_configuration,
+    document_individual,
+    get_weights,
+)
+from prodsim.util.util import set_seed
+
+sim.VERBOSE = 0
+
+class ProductionSystemOptimization(Annealer):
+    def __init__(self, base_configuration: adapters.Adapter, save_folder: str, performances: dict, solutions_dict: dict, start: float, weights: tuple, initial_solution: adapters.Adapter=None):
+        if initial_solution:
+            super().__init__(initial_solution, None)
+        else:
+            super().__init__(base_configuration, None)
+        self.save_folder = save_folder
+        self.base_configuration = base_configuration
+        self.performances = performances
+        self.solution_dict = solutions_dict
+        self.start = start
+        self.weights = weights
+
+    def move(self):
+        while True:
+            configuration = mutation(individual=[deepcopy(self.state)])[0][0]
+            if check_valid_configuration(
+                configuration=configuration,
+                base_configuration=self.base_configuration,
+            ):
+                self.state = configuration
+                break
+
+    def energy(self):
+
+        values = evaluate(
+            base_scenario=self.base_configuration,
+            performances=self.performances,
+            solution_dict=self.solution_dict,
+            individual=[self.state],
+        )
+
+        performance = sum([value * weight for value, weight in zip(values, self.weights)])
+        # print("\n\t########## Evaluted ind", self.counter, "for value:", performance)
+        counter = len(self.performances["0"]) - 1
+        document_individual(self.solution_dict, self.save_folder, [self.state])
+        self.performances["0"][str(counter)] = {
+            "agg_fitness": performance,
+            "fitness": [float(value) for value in values],
+            "time_stamp": time.perf_counter() - self.start,
+        }
+        with open(f"{self.save_folder}/optimization_results.json", "w") as json_file:
+            json.dump(self.performances, json_file)
+
+        return performance
+
+def run_simulated_annealing(
+    save_folder: str,
+    base_configuration_file_path: str,
+    scenario_file_path: str,
+    seed: int,
+    Tmax: int,
+    Tmin: int,
+    steps: int,
+    updates: int,
+    initial_solution_file_path: str = ""
+):
+    base_configuration = adapters.JsonAdapter()
+    base_configuration.read_data(base_configuration_file_path, scenario_file_path)
+
+    if initial_solution_file_path:
+        initial_solution = adapters.JsonAdapter()
+        initial_solution.read_data(initial_solution_file_path, scenario_file_path)
+    else:
+        initial_solution = None
+
+    set_seed(seed)
+
+    weights = get_weights(base_configuration, "max")
+
+    solution_dict = {"current_generation": "0", "0": []}
+    performances = {}
+    performances["0"] = {}
+    start = time.perf_counter()
+
+    pso = ProductionSystemOptimization(
+        base_configuration=base_configuration,
+        save_folder=save_folder,
+        performances=performances,
+        solutions_dict=solution_dict,
+        start=start,
+        weights=weights,
+        initial_solution=initial_solution)
+
+    pso.Tmax = Tmax
+    pso.Tmin = Tmin
+    pso.steps = steps
+    pso.updates = updates
+
+    internary, performance = pso.anneal()
