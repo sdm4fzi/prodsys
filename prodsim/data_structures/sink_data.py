47799d663c2fab3dad87a85069cb8721ba7153a8
SebBehrendtC:/Users/Sebas/OneDrive/Documents/venvs/simpy_venv/Scripts/python.exe <sebastian.behrendt@kit.edu>
2023-04-11 22:21:20 +0200

    Adjusted tuple definition of location - fastAPIbug
    Former-commit-id: 509dd58c933e7e249f0fc07400a864e705f092e6

--- prodsim/data_structures/sink_data.py
+++ prodsim/data_structures/sink_data.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 from typing import Literal, Union, List, Tuple, Optional
 
-from pydantic import validator
+from pydantic import validator, conlist
 
 from prodsim.data_structures.core_asset import CoreAsset
 
 
 class SinkData(CoreAsset):
-    location: Tuple[float, float]
+    location: conlist(float, min_items=2, max_items=2)
     material_type: str
     input_queues: List[str]
 
