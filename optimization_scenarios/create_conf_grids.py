477ccf8c708103d40cce1fcd546ad05664366247
SebBehrendtC:/Users/Sebas/OneDrive/Documents/venvs/simpy_venv/Scripts/python.exe <sebastian.behrendt@kit.edu>
2023-04-21 00:20:12 +0200

    Renaming optimization_scenarios
    Former-commit-id: e8409418e6e31ec48f5a004c4f010e3cac69e878

--- optimization_scenarios/create_conf_grids.py
+++ optimization_scenarios/create_conf_grids.py
@@ -1,4 +1,5 @@
 import yaml
+import pandas as pd
 
 SCENARIO = "H1"
 
@@ -14,7 +15,7 @@ def create_hyperparameter_conf_and_save(
         "crossover_rate": crossover_rate,
         "number_of_processes": number_of_processes,
     }
-    with open("optimizationonf/optimization/" + SCENARIO + "_" + filename + ".yaml", "w") as f:
+    with open("optimization_scenarios/conf/optimization/" + SCENARIO + "_" + filename + ".yaml", "w") as f:
         yaml.dump(conf, f)
 
 
@@ -23,21 +24,26 @@ def create_general_conf(filename):
         "mode": "optimization",
         "configuration_path": "base_configuration_" + SCENARIO + ".json",
         "scenario_path": "scenario_" + SCENARIO + ".json",
-        "save_folder": "results/" + SCENARIO,
+        "save_folder": "results/" + SCENARIO + "_" + filename,
     }
-    with open("conf/general/" + SCENARIO + "_" + filename + ".yaml", "w") as f:
+    with open("optimization_scenarios/conf/general/" + SCENARIO + "_" + filename + ".yaml", "w") as f:
         yaml.dump(conf, f)
 
 
 algorithm = "evolutionary"
 seed = 22
-number_of_generations = [20, 30, 40, 50, 60]
+number_of_generations = [20, 40, 60]
 number_of_individuals = [64, 128, 256, 512]
-mutation_rates = [0.01, 0.05, 0.1, 0.2]
-crossover_rates = [0.01, 0.05, 0.1, 0.2]
+mutation_rates = [0.05, 0.15]
+crossover_rates = [0.05, 0.15]
 number_of_processes = 16
 
 configurations = []
+
+df = pd.DataFrame(columns=["number_of_generation", "number_of_individual", "mutation_rate", "crossover_rate", "filename"])
+
+
+grid_runner_string = "ECHO Welcome to optimization of " + SCENARIO + "\n"
 
 for number_of_generation in number_of_generations:
     for number_of_individual in number_of_individuals:
@@ -57,3 +63,16 @@ for number_of_generation in number_of_generations:
                     crossover_rate,
                 )
                 create_general_conf(hyper_param_name)
+                df.loc[len(df)] = [
+                    number_of_generation,
+                    number_of_individual,
+                    mutation_rate,
+                    crossover_rate,
+                    hyper_param_name,
+                ]
+
+                grid_runner_string += "CALL python hydra_app.py general=" + SCENARIO + "_" + hyper_param_name + " optimization=" + SCENARIO + "_" + hyper_param_name + "\n"
+                grid_runner_string += "ECHO Finished optimization of " + hyper_param_name + "\n"
+df.to_excel("hyper_parameter_grid_" +  SCENARIO + ".xlsx")
+with open("optimization_scenarios/grid_runner_" + SCENARIO + ".bat", "w") as f:
+    f.write(grid_runner_string)
