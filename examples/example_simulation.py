85556d2e7de49a9b5b46c8aea75d20d6ef2c0738
SebBehrendt <sebastian.behrendt@kit.edu>
2023-12-14 23:47:55 +0100

    added FIXMEs

--- examples/example_simulation.py
+++ examples/example_simulation.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
 
     adapter_object = prodsys.adapters.JsonProductionSystemAdapter()
 
-    adapter_object.read_data('examples/basic_example/example_configuration.json')
+    # adapter_object.read_data('examples/basic_example/example_configuration.json')
+    adapter_object.read_data('examples/tutorials/control_configuration.json')
     # adapter_object.write_data("data/example_configuration.json")
-
+    # FIXME: Limited queue sizes are not working correctly in example_configuration. Fix the issue in the error handling. 
+    # FIXME: control configuration does not use R4. Fix the issue in the error handling.
 
     runner_object = prodsys.runner.Runner(adapter=adapter_object)
     runner_object.initialize_simulation()
