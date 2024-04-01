84357014fb32407c2bf5ffa9f9831b433ff5b0c1
TassiloHoltzwartKenbun <132047653+TassiloHoltzwartKenbun@users.noreply.github.com>
2024-04-01 22:59:57 +0200

    added nearest origin policies to resource factory
    Add Nearest Origin control policies

--- prodsys/factories/resource_factory.py
+++ prodsys/factories/resource_factory.py
@@ -35,6 +35,9 @@ CONTROL_POLICY_DICT: Dict = {
     ResourceControlPolicy.LIFO: control.LIFO_control_policy,
     ResourceControlPolicy.SPT: control.SPT_control_policy,
     TransportControlPolicy.SPT_transport: control.SPT_transport_control_policy,
+    TransportControlPolicy.Nearest_origin_and_longest_target_queues_transport : control.Nearest_origin_and_longest_target_queues_transport_control_policy,
+    TransportControlPolicy.Nearest_origin_and_shortest_target_input_queues_transport : control.Nearest_origin_and_shortest_target_input_queues_transport_control_policy,
+
 }
 
 
