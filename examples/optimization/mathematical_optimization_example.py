15fd28213a98cbdfa5bd55e2ff550a4a1a6da558
Sebastian Behrendt <sebastianbehrendt97@gmail.com>
2024-08-13 16:13:24 +0200

    removed  import bugs in mathematical optimization

--- examples/optimization/mathematical_optimization_example.py
+++ examples/optimization/mathematical_optimization_example.py
@@ -4,7 +4,7 @@ from prodsys.optimization.math_opt import run_mathematical_optimization
 BASE_CONFIGURATION_FILE_PATH = "examples/optimization/optimization_example/base_scenario.json"
 SCENARIO_FILE_PATH = "examples/optimization/optimization_example/scenario.json"
 SAVE_FOLDER = "data/math_results"
-OPTIMIZATION_TIME_PORTION = 1.0
+OPTIMIZATION_TIME_PORTION = 0.1
 NUMBER_OF_SOLUTIONS = 2
 ADJUSTED_NUMBER_OF_TRANSPORT_RESOURCES = 2
 NUMBER_OF_SEEDS = 2
