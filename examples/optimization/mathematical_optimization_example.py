dc28eef249fbc796caf6b51c397bc269a9071545
Sebastian Behrendt <sebastianbehrendt97@gmail.com>
2024-08-13 17:57:13 +0200

    updated inconsistent use of literal and enum in scenario data options

--- examples/optimization/mathematical_optimization_example.py
+++ examples/optimization/mathematical_optimization_example.py
@@ -4,7 +4,7 @@ from prodsys.optimization.math_opt import run_mathematical_optimization
 BASE_CONFIGURATION_FILE_PATH = "examples/optimization/optimization_example/base_scenario.json"
 SCENARIO_FILE_PATH = "examples/optimization/optimization_example/scenario.json"
 SAVE_FOLDER = "data/math_results"
-OPTIMIZATION_TIME_PORTION = 0.25
+OPTIMIZATION_TIME_PORTION = 1
 NUMBER_OF_SOLUTIONS = 1
 ADJUSTED_NUMBER_OF_TRANSPORT_RESOURCES = 2
 NUMBER_OF_SEEDS = 2
