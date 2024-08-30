40b76b653247853db62c7cb7b18ab13db38dc4be
johannadanz <johanna_danz@live.de>
2024-08-30 15:28:12 +0800

    Fixed buf with location of node for input and output location

--- prodsys/simulation/node.py
+++ prodsys/simulation/node.py
@@ -16,3 +16,21 @@ class Node(BaseModel):
             List[float]: The location of the resource. Has to have length 2.
         """
         return self.data.location
+    
+    def get_input_location(self) -> List[float]:
+        """
+        Returns the location of the resource.
+
+        Returns:
+            List[float]: The location of the resource. Has to have length 2.
+        """
+        return self.data.location
+    
+    def get_output_location(self) -> List[float]:
+        """
+        Returns the location of the resource.
+
+        Returns:
+            List[float]: The location of the resource. Has to have length 2.
+        """
+        return self.data.location
