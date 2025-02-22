36696465e1175ec79a6d659ff05c2689bdec210a
Sebastian Behrendt <sebastianbehrendt97@gmail.com>
2025-02-22 16:32:10 +0100

    updated scenario time range

--- docker_runner.bat
+++ docker_runner.bat
@@ -1,5 +1,5 @@
-@REM docker build -t sebbehrendt/prodsys .
-@REM docker run -d --name prodsys -p 8000:8000 sebbehrendt/prodsys
+docker build -t sebbehrendt/prodsys .
+docker run -d --name prodsys -p 8000:8000 sebbehrendt/prodsys
 @REM docker push sebbehrendt/prodsys
 @REM docker pull sebbehrendt/prodsys
 @REM docker run -d --name prodsys -p 8000:8000 sebbehrendt/prodsys
