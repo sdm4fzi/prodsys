42652797033bc32044423db6f26f877af802d913
SebBehrendt <sebastianbehrendt97@gmail.com>
2025-11-28 20:40:20 -0500

    removed unneeded files

--- docker_runner.bat
+++
@@ -1,5 +0,0 @@
-docker build -t sebbehrendt/prodsys .
-docker run -d --name prodsys -p 8000:8000 sebbehrendt/prodsys
-@REM docker push sebbehrendt/prodsys
-@REM docker pull sebbehrendt/prodsys
-@REM docker run -d --name prodsys -p 8000:8000 sebbehrendt/prodsys
